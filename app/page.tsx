3d3c14a7f9b426284798dd6efc3dc298650e6e0f
HAEAH <haeah@HAEAHs-MacBook-Air-2.local>
2026-09-21 20:56:19 -0400

    Display movies in table format

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,31 @@ export default async function Home() {
 
             <LoginButton />
 
-            <ul>
+            <table style={{ borderCollapse: "collapse", marginTop: "20px" }}>
+                <thead>
+                <tr>
+                    <th style={{ padding: "10px 20px", borderBottom: "2px solid #ccc", textAlign: "left" }}>
+                        Movie
+                    </th>
+                    <th style={{ padding: "10px 20px", borderBottom: "2px solid #ccc", textAlign: "left" }}>
+                        Year
+                    </th>
+                </tr>
+                </thead>
+
+                <tbody>
                 {movies?.map((movie) => (
-                    <li key={movie.id}>
-                        {movie.title} ({movie.year})
-                    </li>
+                    <tr key={movie.id}>
+                        <td style={{ padding: "10px 20px", borderBottom: "1px solid #ddd" }}>
+                            {movie.title}
+                        </td>
+                        <td style={{ padding: "10px 20px", borderBottom: "1px solid #ddd" }}>
+                            {movie.year}
+                        </td>
+                    </tr>
                 ))}
-            </ul>
+                </tbody>
+            </table>
         </main>
     );
 }
